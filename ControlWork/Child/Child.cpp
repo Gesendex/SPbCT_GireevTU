35e90341ea53b8a001c5de4aef2987b09b1a3e8d
Gesendex <73191987+Gesendex@users.noreply.github.com>
2020-11-03 19:32:43 +0300

    Add ControlWork

---
+++ ControlWork/Child/Child.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Child process start" << endl;
+	cout << "Number aguments " << argc << endl;
+	cout << "Arguments:" << endl;
+	
+	for (int i = 0; i < argc; i++)
+	{
+		cout << "argv["<< i <<"] = " << argv[i] << endl;
+	}
+	cout << "Child process end" << endl;
+	exit(0);
+}
---
+++ ControlWork/Parent/Parent.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	STARTUPINFO sti = { 0 };
+	PROCESS_INFORMATION pi = { 0 };
+	DWORD excode;
+
+	wstring ApplicationName(L"C:\\SP\\1\\Parent\\Debug\\Child.exe ");
+	LPWSTR lpwAppName = &ApplicationName[0];
+
+	wstring CommandLine(L"file.txt file2.txt");
+	LPWSTR lpwCmdLine = &CommandLine[0];
+	cout << "Parent process start" << endl;
+	if (!CreateProcess
+	(
+		lpwAppName,
+		lpwCmdLine,
+		NULL,
+		NULL,
+		TRUE,
+		NULL,
+		NULL,
+		NULL,
+		&sti,
+		&pi
+	))
+	{
+		cout << "Unable to generate process" << endl;
+		return -1;
+	}
+
+	_getch();
+
+	GetExitCodeProcess(pi.hProcess, &excode);
+	cout << excode << endl;
+
+	if (excode != STILL_ACTIVE)
+	{
+		cout << "Done" << endl;
+	}
+	else
+	{
+		cout << "Process still running" << endl;
+	}
+	cout << "Parent process end" << endl;
+
+	_getch();
+}
