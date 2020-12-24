5aab3717afb66130d4a01cb8101b0ef7fa01fde6
Gesendex <73191987+Gesendex@users.noreply.github.com>
2020-12-24 22:34:26 +0300

    Практика

---
+++ EducationalPractice/EducationalPracticeDLL/EducationalPracticeDLL/MathLibrary.h
@@ -0,0 +1,18 @@
+// MathLibrary.h - Contains declarations of math functions
+#pragma once
+#include <vector>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <string>
+
+#ifdef EDUCATIONALPRACTICEDLL_EXPORTS
+#define EDUCATIONALPRACTICEDLL_API __declspec(dllexport)
+#else
+#define EDUCATIONALPRACTICEDLL_API __declspec(dllimport)
+#endif
+
+
+extern "C" EDUCATIONALPRACTICEDLL_API void ATTRIBUTE(LPSTR a);
+
+
